fa5a0ecdef9497a7d8c67814d586684f284c7728
Siti Ayuni <92244072+sitiayuni@users.noreply.github.com>
2022-06-15 13:19:58 +0700

    update

--- uappetruk.cpp
+++ uappetruk.cpp
@@ -169,7 +169,9 @@ void lionair(){
 	gotoxy (55,11); cout << "2. JT 123";
 	gotoxy (49,12);  cout <<	"<><><><><><><><><><><>";
 	gotoxy (50,14); cout << "No. Penerbangan : "; cin >> np;
-case 1 :
+	switch (np){
+	
+		case 1 :
 			while(1){
 			system("cls");
 			lambangLion();
@@ -181,4 +183,11 @@ case 1 :
 			gotoxy(45,13);  cout << "|   Program Bagasi Pesawat   |";
 			gotoxy(45,14);  cout << "------------------------------";
 			gotoxy(45,15);  cout << "------------------------------";
+			gotoxy(45,16);  cout << "| 1. Masukkan Koper          |";
+			gotoxy(45,17);  cout << "| 2. Keluarkan Koper         |";
+			gotoxy(45,18);  cout << "| 3. Tampilkan Jumlah Koper  |";
+			gotoxy(45,19);  cout << "| 4. Selesai                 |";
+			gotoxy(45,20);  cout << "| 5. Keluar                  |";
+			gotoxy(45,21);  cout << "------------------------------";
+			gotoxy(45,22);  cout << "Masukkan pilihan : ";	
 
