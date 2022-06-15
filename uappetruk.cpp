7801747aba7399478f09b15935a12338a4b19110
Shafa Auliya <92239898+ShafaAuliya21@users.noreply.github.com>
2022-06-15 13:39:48 +0700

    Update uappetruk.cpp

--- uappetruk.cpp
+++ uappetruk.cpp
@@ -284,4 +284,25 @@ void batikair(){
 			}
 		}
 		break;
+		
+		case 2 :
+			while(1){
+			system("cls");
+			lambangBatik();
+			setcolor(7);
+			gotoxy (45,7);  cout << "No. Penerbangan 	 : ID 6351";
+			gotoxy (45,8);  cout << "Tujuan 		 : Jakarta (CGK)";
+			gotoxy (45,9);  cout << "Waktu Keberangkatan : 13.20 WIB";
+			gotoxy(45,12);  cout << "------------------------------";
+			gotoxy(45,13);  cout << "|   Program Bagasi Pesawat   |";
+			gotoxy(45,14);  cout << "------------------------------";
+			gotoxy(45,15);  cout << "------------------------------";
+			gotoxy(45,16);  cout << "| 1. Masukkan Koper          |";
+			gotoxy(45,17);  cout << "| 2. Keluarkan Koper      	  |";
+			gotoxy(45,18);  cout << "| 3. Tampilkan Jumlah Koper  |";
+			gotoxy(45,19);  cout << "| 4. Selesai                 |";
+			gotoxy(45,20);  cout << "| 5. Keluar                  |";
+			gotoxy(45,21);  cout << "------------------------------";
+			gotoxy(45,22);  cout << "Masukkan pilihan : ";
+			cin >> pilihan; 
 	
