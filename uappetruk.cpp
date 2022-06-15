85c179193d0291b4b6afed37fea21e27e311f974
Jihan Haya Mufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2022-06-15 12:58:41 +0700

    penambahan tabel fungsi displayl1

--- uappetruk.cpp
+++ uappetruk.cpp
@@ -71,6 +71,11 @@ void displayl1(stack<int> kode){
 	while (!kode.empty()){
 		lambangLion();
 			setcolor(7);
+			gotoxy(20,8);  cout << "================================================================================";
+			gotoxy(20,9);  cout << "||    Tanggal    ||   Nomor Penerbangan   ||      Tujuan      ||   No.Koper   ||";
+			gotoxy(20,10); cout << "================================================================================";
+			gotoxy(20,11); cout << "||               ||                       ||                  ||              ||";   
+			gotoxy(20,12); cout << "================================================================================";
 	   		gotoxy(24,11); cout << tgl;
 	   		gotoxy(47,11); cout << "JT 175";
 	   		gotoxy(67,11); cout << "Jakarta (CGK)";
